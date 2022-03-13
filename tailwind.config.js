91c6c540e6c712a0f6dafef1f8f017273215acfc
John Mwakalinga <mwakalingajohn@gmail.com>
2022-03-13 11:19:05 +0300

    - add design implementation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        whitesmoke: "#fdf7f2",
+        primary: "#52784f"
+      }
+    },
+  },
+  plugins: [],
+}
